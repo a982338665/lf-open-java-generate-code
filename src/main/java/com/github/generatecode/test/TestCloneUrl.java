4e5d06e8d6d8d62db59c54ec6c644329c45a6ff3
luofeng <982338665@qq.com>
2021-07-20 23:58:07 +0800

    clone某个用户下所有的git仓库

--- src/main/java/com/github/generatecode/test/TestCloneUrl.java
+++ src/main/java/com/github/generatecode/test/TestCloneUrl.java
@@ -2,6 +2,7 @@ package com.github.generatecode.test;
 
 import com.github.generatecode.model.MatchKeywordStartToEnd;
 import com.github.generatecode.util.RegexMatches;
+import com.github.generatecode.util.StringUtils;
 import jdk.nashorn.internal.ir.LiteralNode;
 
 import java.io.BufferedReader;
@@ -10,6 +11,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author : Mr huangye
@@ -20,47 +23,61 @@ import java.util.List;
 public class TestCloneUrl {
 
     public static void main(String[] args) {
-        //$ curl "https://api.github.com/users/a982338665/repos?per_page=100" >xxx2.txt^  获取该用户下所有的仓库信息
+        //1.获取github用户下的所有仓库信息
+        String exec = exec("curl \"https://api.github.com/users/a982338665/repos?per_page=100\" >xxx2.txt");
+        //2.通过正则匹配获取到clone地址并打印
+        printGitCloneCMD(exec);
+        //3.打开cmd窗口在指定的文件夹下 执行以上clone命令
+    }
+    /**
+     * 打印git clone命令
+     */
+    public static void printGitCloneCMD(String text) {
         StringBuilder listsre = new StringBuilder();
-//        exec("curl \"https://api.github.com/users/a982338665/repos?per_page=100\" >xxx2.txt");
-        List<MatchKeywordStartToEnd> list = RegexMatches.matchKeywordStartToEnd("\"clone_url\": \"", "\"", TestClone.xxx.toString());
+        List<MatchKeywordStartToEnd> list = matchKeywordStartToEnd("\"clone_url\": \"", "\"", text);
         list.forEach(e -> {
-//            exec("git clone --depth=1 " + e.getKeyword());
             listsre.append("git clone --depth=1 " + e.getKeyword() + "\n");
         });
-        List<MatchKeywordStartToEnd> list2 = RegexMatches.matchKeywordStartToEnd("\"clone_url\": \"", "\"", TestClone.xxx2.toString());
-        list2.forEach(e -> {
-            listsre.append("git clone --depth=1 " + e.getKeyword() + "\n");
-
-//            exec("git clone --depth=1 " + e.getKeyword());
-        });
-        List<MatchKeywordStartToEnd> list3 = RegexMatches.matchKeywordStartToEnd("\"clone_url\": \"", "\"", TestClone.xxx3.toString());
-        list3.forEach(e -> {
-            listsre.append("git clone --depth=1 " + e.getKeyword() + "\n");
-
-//            exec("git clone --depth=1 " + e.getKeyword());
-        });
-//        System.err.println(list.size() + list2.size() + list3.size());
-//        exec();
         System.err.println(listsre.toString());
     }
 
-    public static void exec(String cmd) {
-        // Java调用 dos命令
-//        String cmd = "ping www.baidu.com";
+    public static String exec(String cmd) {
         try {
             Process process = Runtime.getRuntime().exec(cmd);
             InputStream is = process.getInputStream();
             InputStreamReader isr = new InputStreamReader(is, "GBK");
             BufferedReader br = new BufferedReader(isr);
+            StringBuilder stringBuilder = new StringBuilder();
             String content = br.readLine();
             while (content != null) {
-                System.out.println(content);
+//                System.out.println(content);
+                stringBuilder.append(content);
                 content = br.readLine();
             }
+            return stringBuilder.toString();
+//            System.err.println(stringBuilder.toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return null;
     }
 
+    public static List<MatchKeywordStartToEnd> matchKeywordStartToEnd(String start, String end, String text) {
+        List<MatchKeywordStartToEnd> list = new ArrayList<>();
+        String regex = getRegex(start, end);
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(text); // 获取 matcher 对象
+        while (m.find()) {
+            String str = text.substring(m.start(), m.end());
+            //(?<scope>.*?)是用于标识scope的方式，表示夹在中间的字符串都叫scope，可以通过matcher.group("scope")提取。
+            //[\\s\\S]*? 是用来匹配任意字符，表示在前后有界定的情况下（例如夹在<xxx></xxx>中间的字符串），可以忽略那些不规则字符串的匹配。
+            list.add(new MatchKeywordStartToEnd(m.start(), m.end(), m.group("scope"), str));
+        }
+        return list;
+    }
+
+    public static String getRegex(String start, String end) {
+//        return StringUtils.concat("\\",start,"(?<scope>.*?)", end);           //不支持跨行匹配
+        return StringUtils.concat("\\", start, "(?<scope>[\\s\\S]*?)", end);      //支持跨行匹配
+    }
 }
